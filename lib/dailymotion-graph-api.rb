7ca6d09b72c79d5dc93ecb9aabddef33bc9abb8f
adrien <adrien.montfort@gmail.com>
2014-05-21 15:35:05 +0200

    User video list

--- lib/dailymotion-graph-api.rb
+++ lib/dailymotion-graph-api.rb
@@ -1,4 +1,4 @@
-require "dailymotion-graph-api/version"
+require 'dailymotion-graph-api/version'
 require 'faraday'
 require 'json'
 
@@ -24,101 +24,84 @@ module DailymotionGraphApi
     end
 
     def refresh_token
-      result = connexion.post do |request|
-        request.url     '/oauth/token'
-        request.body  = {
-          grant_type:     'refresh_token',
-          client_id:      @client_id,
-          client_secret:  @client_secret,
-          refresh_token:  @refresh_token
-        }
-      end
-
-      r = JSON.parse(result.body)
-      if r.keys.include?('error')
-        raise "#{r['error']}: #{r['error_description']}"
-      end
-
-      @access_token   = r['access_token']
-      @refresh_token  = r['refresh_token']
-
-      r
+      token('refresh_token')
     end
 
     def get_access_token(code, callback_url)
-      result = connexion.post do |request|
-        request.url     '/oauth/token'
-        request.body  = {
-          grant_type:     'authorization_code',
-          client_id:      @client_id,
-          client_secret:  @client_secret,
-          redirect_uri:   callback_url,
-          code:           code
-        }
-      end
-
-      r = JSON.parse(result.body)
-      if r.keys.include?('error')
-        raise "#{r['error']}: #{r['error_description']}"
-      end
-
-      @access_token   = r['access_token']
-      @refresh_token  = r['refresh_token']
-
-      r
+      token('authorization_code', code, callback_url)
     end
-    
+
     def tmp_upload(video_path)
       remote_uri = URI.parse(get_upload_url['upload_url'])
-      
+
       upload_connexion = Faraday.new(:url => 'http://upload-01.dailymotion.com', :ssl => {:verify => false}) do |faraday|
         faraday.request  :multipart
         faraday.response :logger
         faraday.adapter  Faraday.default_adapter
       end
-      
-      result = upload_connexion.post do |request|
-        request.url     '/upload'
-        request.params  = remote_uri.query.split('&').collect{|item| item.split('=')}.to_h # surement plus élégant à faire
-        request.body    = {
-          file: Faraday::UploadIO.new(video_path, 'video/mp4')
-        }
-      end
-      
-      r = JSON.parse(result.body)
-      if r.keys.include?('error')
-        raise "#{r['error']}: #{r['error_description']}"
-      end
 
-      r
+      send_request('/upload', remote_uri.query.split('&').collect{|item| item.split('=')}.to_h.merge(
+          { body: {
+              file: Faraday::UploadIO.new(video_path, 'video/mp4')
+            },
+            connexion: upload_connexion
+          })
+        )
     end
-    
+
     def create_video(arguments)
-      result = connexion.post do |request|
-        request.url     '/me/videos'
-        request.params  = {
-          access_token:   @access_token
-        }
-        request.body    = arguments
-      end
-      
-      r = JSON.parse(result.body)
-      if r.keys.include?('error')
-        raise "#{r['error']}: #{r['error_description']}"
-      end
-
-      r
+      send_request('/me/videos', { verb: :post, body: arguments })
     end
-    
-    
+
+    def videos(user, fields = %w(id title created_time))
+      has_more = true
+      page = 1
+      result = []
+      while has_more
+        response = send_request("/user/#{user}/videos", limit: 100, page: page, fields: fields.join(','))
+        result += response['list']
+        has_more = response['has_more']
+        page += 1
+      end
+      result
+    end
+
     def get_upload_url
-      result = connexion.get do |request|
-        request.url     '/file/upload'
-        request.params  = {
-          access_token:   @access_token
-        }
+      send_request('/file/upload')
+    end
+
+    def get_infos(user, additional_params = {})
+      send_request("/user/#{user}", additional_params)
+    end
+
+    private
+
+    DEFAULTS = {
+        verb: :get,
+        auth: true
+    }
+
+    def send_request(url, options = {})
+      options = DEFAULTS.merge(options)
+      verb = options.delete(:verb)
+      body = options.delete(:body)
+      auth = options.delete(:auth)
+      connexion = options.delete(:connexion) || self.connexion
+
+      if auth
+        if @access_token
+          options[:access_token] = @access_token
+        else
+          raise 'Access token is required'
+        end
       end
-      
+
+      result = connexion.send(verb) do |request|
+        request.url     url
+        request.params = options
+        request.body = body if body
+      end
+
       r = JSON.parse(result.body)
       if r.keys.include?('error')
         raise "#{r['error']}: #{r['error_description']}"
@@ -126,21 +109,18 @@ module DailymotionGraphApi
 
       r
     end
-    
-    def get_infos(user, additional_params = {})
-      raise "Access token is required" if @access_token.nil?
 
-      result = connexion.get do |request|
-        request.url     "/user/#{user}"
-        request.params  = {
-          access_token:   @access_token
-        }.merge(additional_params)
-      end
+    def token(grant_type, code = nil, callback_url = nil)
+      r = send_request('/oauth/token', { verb: :post, auth: false, body: {
+          grant_type:     grant_type,
+          client_id:      @client_id,
+          client_secret:  @client_secret,
+          redirect_uri:   callback_url,
+          code:           code
+        } })
 
-      r = JSON.parse(result.body)
-      if r.keys.include?('error')
-        raise "#{r['error']}: #{r['error_description']}"
-      end
+      @access_token   = r['access_token']
+      @refresh_token  = r['refresh_token']
 
       r
     end
